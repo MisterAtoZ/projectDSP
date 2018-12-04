c2a139fd31bf652197b74e872269fc846028a790
MisterAtoZ <jonathan.luijsmans@student.uhasselt.be>
2018-12-04 19:55:52 +0100

    toevoegen van verschillende notch filters en testen ermee

--- main.m
+++ main.m
@@ -41,16 +41,87 @@ Xcomp = imag(X);
 absX = abs(X);
 N = length(absX);
 figure
-subplot(1,2,1)
+subplot(1,3,1)
 plot(absX)
 title("FFT van signal1");
 xlabel("Frequentie (Hz)");
 
 %ingezoomde FFT
-subplot(1,2,2);
+subplot(1,3,2);
 axis([0,100,0,1.1*max(absX)]);
 plot(absX(1:round(N/10)))
 title("Ingezoomt tussen 0Hz en 100Hz");
 xlabel("Frequentie (Hz)");
 
+%Notch filter zelf
+fs = 1000;
+f0 = 50;
+notchWidth = 0.01;
+lnotch = notch(signal1, fs, f0, notchWidth);
+mnotch = notch(lnotch, fs, 60, notchWidth);
+Xnotch = fft(mnotch);
+absXnotch = abs(Xnotch);
+hold on
+%axis([0,100,0,1.1*max(abs(Xnotch))]);
+plot(absXnotch(1:round(N/10)))
+title("Ingezoomt tussen 0Hz en 100Hz, met notch filter");
+xlabel("Frequentie (Hz)");
+legend();
 
+%FFT met notch maken
+%subplot(1,3,3);
+figure
+axis([0,2500,1.1*ymin1,1.1*ymax1]); %waardes aanpassen
+plot(absX)
+title("FFT van signal1");
+xlabel("Frequentie (Hz)");
+hold on
+plot(absXnotch)
+legend();
+
+%notch filter op signaal in de tijd
+figure
+plot(time1,signal1)
+axis([0,totaltime1,1.1*ymin1,1.1*ymax1]);
+title("Het originele signaal en het genotchte signaal");
+xlabel("Tijd in s");
+hold on
+plot(time1, mnotch)
+legend();
+
+%notch filter van matlab
+wo = 60/(fs1/2); %deze filter lijkt zelfs niet te werken 
+bw = wo/35;
+[c d] = iirnotch(wo,bw);
+
+nnotch = filter(c,d,signal1);
+figure 
+plot(time1,signal1)
+axis([0,totaltime1,1.1*ymin1,1.1*ymax1]);
+title("Het originele signaal en het 2de genotchte signaal");
+xlabel("Tijd in s");
+hold on
+plot(time1, nnotch)
+legend();
+
+%FFT van matlabnotch volledig
+Ynotch = fft(nnotch);
+absYnotch = abs(Ynotch);
+figure
+plot(absX)
+title("FFT van signal1, met de notch van matlab");
+xlabel("Frequentie (Hz)");
+hold on
+plot(absYnotch)
+legend();
+
+%FFT van matlabnotch ingezoomed
+figure
+axis([0,100,0,1.1*max(absX)]);
+plot(absX(1:round(N/10)))
+title("Ingezoomt tussen 0Hz en 100Hz");
+xlabel("Frequentie (Hz)");
+hold on
+%axis([0,100,0,1.1*max(abs(Xnotch))]);
+plot(absYnotch(1:round(N/10)))
+legend();
